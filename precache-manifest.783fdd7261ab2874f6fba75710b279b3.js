28e19d3af83d1780d45ca831ec8cd93a4957afe1
徐鑫 <15204234251@163.com>
2020-03-22 17:28:33 +0800

    测试

---
+++ precache-manifest.783fdd7261ab2874f6fba75710b279b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303c2ca5582563dc05febfcef9b3f6c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad32124343112fe2a435",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260c646ebe3b4a83c9d4",
+    "url": "./static/js/2.d8c10116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d8c10116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad32124343112fe2a435",
+    "url": "./static/js/main.f281d4f4.chunk.js"
+  },
+  {
+    "revision": "f2fd9e8b168d5abefa9c",
+    "url": "./static/js/runtime-main.2577c369.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
